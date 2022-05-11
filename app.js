42ebd933a577535ab10142767f5ab42b061dd6a7
Makhmudbek10 <103061057+Makhmudbek10@users.noreply.github.com>
2022-05-11 13:18:50 +0300

    js17
    js17

---
+++ app.js
@@ -0,0 +1,73 @@
+// let name = prompt('What is your name?')
+// let surname = prompt('What is your surname?')
+// let age = prompt('how old are you nigger?')
+
+// let eMail = [
+//     `${name}${surname}${age}@mail.com`,
+//     `${surname}${age}${name}@mail.com`,
+//     `${age}${name}${surname}@mail.com`,
+//     `${name}${age}${surname}@mail.com`,
+//     `${age}${surname}${name}@mail.com`,
+
+// ]
+// console.log(eMail);
+
+
+// let randomEmail = () =>{
+//     let position = Math.floor(Math.random() * 3)
+//     alert(eMail[position])
+// }
+// randomEmail()
+
+
+// function randomid() {
+//     let a = ''
+//     let idtext = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+//     let maxposition = idtext.length - 1;
+//     for(i = 0; i <= 7; ++i) {
+//         let position = Math.floor (Math.random() * maxposition)
+//         a = a + idtext.slice(position, position + 1)
+//     }
+//     console.log(a)
+//     return a;
+// }
+
+
+// let id = [45, 3, 22, 97, 7]
+
+// let sort = () => {
+//     let sort = []
+//     let f = Infinity
+//     let h = Infinity
+//     for(let i = 0; i < id.length; i++ ) {
+//         for(let k = 0; k < id.length; k++) {
+//             if(id[k] < f) {
+//                 f = id[k]
+//                 h = k
+//             }
+//         }
+//         id[h] = Infinity
+//         sort.push(f)
+//         f = Infinity
+//     }
+// console.log(sort);
+// return sort
+
+// }
+// sort()
+
+
+// // let fib = 0;
+// // let fibs = 1;
+// // let num = prompt()
+
+// // for (let i = 1; i <= num; i++) {
+// //   let wepro = num_one + num_two;
+
+// //   fib = fibs;
+// //   fibs = wepro;
+
+// //   console.log(wepro);
+// // }
+
+
